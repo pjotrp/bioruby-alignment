9ecf0a017195bb3129a786628f249c256223f00c
Pjotr Prins <pjotr.public01@thebird.nl>
2012-02-03 12:44:25 +0100

    Steps

---
+++ features/codon.rb
@@ -0,0 +1,29 @@
+Given /^I read an MSA nucleotide FASTA file in the test\/data folder$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I iterate the sequence records$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I check the alignment$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should contain codons$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should translate to an amino acid MSA$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should write a nucleotide alignment$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should write an amino acid alignment$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
