e73f4df5b9678c2f2f6347365032eed9be32ab63
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-06 12:15:37 +0100

    Factored out the row marker function

--- lib/bio-alignment/edit/del_short_sequences.rb
+++ lib/bio-alignment/edit/del_short_sequences.rb
@@ -1,29 +1,22 @@
+require 'bio-alignment/edit/edit_rows'
 
 module Bio
   module BioAlignment
 
     module DelShortSequences
+      include MarkRows
    
       # Return a new alignment with rows marked for deletion, i.e. mark
       # rows that mostly contain gaps (threshold +percentage+). The 
       # alignment returned is a cloned copy
       def mark_short_sequences percentage = 30
-        aln = self.clone 
-        # clone row state 
-        aln.rows.each do | row |
-          new_state =
-            if row.state
-              row.state.clone
-            else
-              RowState.new
-            end
-          gap_num = row.count { |e| e.gap? }
-          if (gap_num.to_f/row.length) > 1.0-percentage/100.0
-            new_state.delete!
+        mark_rows { |state,row| 
+          num = row.count { |e| e.gap? }
+          if (num.to_f/row.length) > 1.0-percentage/100.0
+            state.delete!
           end
-          row.state = new_state
-        end
-        aln
+          state
+        }
       end
 
       # Return an alignment with the bridges removed
