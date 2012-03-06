24be8f418ea15072894c007b4c17ec1011bac292
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-06 11:51:01 +0100

    Factored out the row marker function

--- lib/bio-alignment/edit/del_non_informative_sequences.rb
+++ lib/bio-alignment/edit/del_non_informative_sequences.rb
@@ -1,29 +1,10 @@
+require 'bio-alignment/edit/edit_rows'
 
 module Bio
   module BioAlignment
 
-    # Function for marking rows (sequences), when a row block returns the new
-    # state, and returning a newly cloned alignment
-    module MarkRow
-      # Mark each seq
-      def mark_rows &block
-        aln = self.clone 
-        # clone row state, or add a state object 
-        aln.rows.each do | row |
-          new_state =
-            if row.state
-              row.state.clone
-            else
-              RowState.new
-            end
-          row.state = block.call(new_state,row)
-        end
-        aln
-      end
-    end
-
     module DelNonInformativeSequences
-      include MarkRow
+      include MarkRows
    
       # Return a new alignment with rows marked for deletion, i.e. mark rows
       # that mostly contain undefined elements and gaps (threshold
