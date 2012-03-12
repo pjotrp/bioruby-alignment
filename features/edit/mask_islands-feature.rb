e94fa1ecb6e332eb3d752fb40695efa4b384ec51
Pjotr Prins <pjotr.public01@thebird.nl>
2012-03-12 08:35:34 +0100

    Delete masked elements

--- features/edit/mask_islands-feature.rb
+++ features/edit/mask_islands-feature.rb
@@ -13,12 +13,13 @@ end
 
 Then /^it should have masked islands$/ do |string|
   check_aln = Alignment.new(string.split(/\n/))
-  new_aln = @marked_aln.update_each_element { |e| (e.state.masked ? Element.new("X"):e)}
-  print new_aln.to_s
+  new_aln = @marked_aln.update_each_element { |e| (e.state.masked? ? Element.new("X"):e)}
   new_aln.to_s.should == check_aln.to_s
 end
 
 Then /^it should also be able to delete islands$/ do |string|
-  pending # express the regexp above with the code you wish you had
+  check_aln = Alignment.new(string.split(/\n/))
+  new_aln = @marked_aln.update_each_element { |e| (e.state.masked? ? Element.new("-"):e)}
+  new_aln.to_s.should == check_aln.to_s
 end
 
