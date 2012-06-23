d5a789a0ae861f8049fc272d4c52e903a07055cd
Pjotr Prins <pjotr.public01@thebird.nl>
2012-06-23 11:45:20 +0200

    Adding step definitions

--- features/phylogeny/split-tree-feature.rb
+++ features/phylogeny/split-tree-feature.rb
@@ -10,3 +10,19 @@ When /^I split out branches with a minimum of (\d+) and maximum of (\d+) sequenc
   pending # express the regexp above with the code you wish you had
 end
 
+When /^I split the tree$/ do |string|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should have found sub\-trees "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I split the tree with a max of (\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should have found low\-homology sub\-tree "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
