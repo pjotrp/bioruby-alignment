c13c631e4f22012a4c81dc4e7bfac4f2f205e29c
Pjotr Prins <pjotr.public01@thebird.nl>
2012-06-24 13:26:15 +0200

    Split tree

--- features/phylogeny/split-tree-feature.rb
+++ features/phylogeny/split-tree-feature.rb
@@ -4,9 +4,18 @@ When /^I split the tree$/ do |string|
   tree = @aln.attach_tree(@tree)
   @aln.extend TreeSplitter
   (aln1,aln2) = @aln.split_on_distance
-  # p "HERE",aln2,"",aln2
-  # aln2.tree.output_newick(indent: false).should == "((seq6:5.3571434,(seq4:4.04762,((seq8:1.1904755,seq5:1.1904755):1.7857151,((seq3:0.0,seq2:0.0):1.1904755,seq1:1.1904755):1.7857151):1.0714293):1.3095236):4.336735);"
-  aln2.size.should == @aln.size/2
+  aln2.size.should == 5
+  @split1 = aln1
+  @split2 = aln2
+end
+
+Then /^I should have found sub\-trees "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
+  @split2.names.join(",").should == arg2
+  @split1.names.join(",").should == arg1
+end
+
+
+Then /^I should have found low\-homology sub\-tree "([^"]*)"$/ do |arg1|
 end
 
 When /^I split out branches with a maximum of (\d+) sequences from$/ do |arg1, string|
@@ -21,15 +30,8 @@ When /^I split out branches with a minimum of (\d+) and maximum of (\d+) sequenc
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^I should have found sub\-trees "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
 When /^I split the tree with a max of (\d+)$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^I should have found low\-homology sub\-tree "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
 
