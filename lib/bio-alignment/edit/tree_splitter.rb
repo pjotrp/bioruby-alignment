f73e0228948080e3db67011976810e8f9cfc94bc
Pjotr Prins <pjotr.public01@thebird.nl>
2012-06-25 10:42:12 +0200

    Slight simplification

--- lib/bio-alignment/edit/tree_splitter.rb
+++ lib/bio-alignment/edit/tree_splitter.rb
@@ -16,35 +16,33 @@ module Bio
         target_size = size/2+1 if not target_size
 
         aln1 = clone
-        # Get the root of the tree
+        # Start from the root of the tree (FIXME: what if there is no root?)
         new_root = aln1.tree.root
         branch = nil
         while new_root
           # find the nearest child (shortest edge)
           near_children = new_root.nearest_children
-          if (near_children.size == 1)
-            new_root = near_children.first
-          else
-            # We have multiple matches, so we are going to split on the 
-            # number of leafs, or we leave it like it is, if the split 
-            # will be too far from the target
-            new_root = near_children.first
-            near_children.each do |c|
-              # find the nearest match
-              if (c.leaves.size-target_size).abs < (new_root.leaves.size-target_size).abs
-                new_root = c 
-              end
+          # We have multiple matches, so we are going to split on the 
+          # number of leafs, or we leave it like it is, if the split 
+          # will be too far from the target
+          new_root = near_children.first
+          near_children.each do |c|
+            next if c == new_root
+            # find the nearest match
+            if (c.leaves.size-target_size).abs < (new_root.leaves.size-target_size).abs
+              new_root = c 
             end
           end
           branch = aln1.tree.clone_subtree(new_root)
           # p [branch.leaves.size,target_size]
+          # Break out of the loop when we hit the target
           break if branch.leaves.size <= target_size 
         end
         reduced_tree = aln1.tree.clone_tree_without_branch(new_root)
         # p branch.map { |n| n.name }.compact
         # p reduced_tree.map { |n| n.name }.compact
 
-        # Now reduce the alignments themselves
+        # Now reduce the alignments themselves to match the trees
         aln1 = tree_reduce(reduced_tree)
         aln2 = tree_reduce(branch)
         return aln1,aln2
